564b1981d2b74afd4fb9d7e58d0a7bb44ac3a7dd
Alexandra <rudakalexx@gmail.com>
2020-07-04 13:51:51 +0300

    Test GHActions

--- src/test/java/ru/netology/manager/FlightOfferManagerTest.java
+++ src/test/java/ru/netology/manager/FlightOfferManagerTest.java
@@ -16,7 +16,7 @@ class FlightOfferManagerTest {
     private FlightOffer MoscowLondon = new FlightOffer(1, 14000, "SVO", "LHR", 243);
     private FlightOffer MoscowParis = new FlightOffer(2, 17000, "SVO", "PAR", 235);
     private FlightOffer MoscowParisSales = new FlightOffer(3, 12750, "SVO", "PAR", 240);
-    private FlightOffer MoscowParisBusiness = new FlightOffer(4, 35000, "SVO", "PAR", 237);
+    private FlightOffer MoscowParisPremium = new FlightOffer(4, 35000, "SVO", "PAR", 237);
     private FlightOffer MoscowPrague = new FlightOffer(5, 13000, "SVO", "PRG", 170);
     private FlightOffer MoscowPragueSales = new FlightOffer(6, 13000, "SVO", "PRG", 155);
     private FlightOffer MoscowBeijing = new FlightOffer(7, 30000, "SVO", "BJS", 430);
@@ -26,7 +26,7 @@ class FlightOfferManagerTest {
         manager.add(MoscowLondon);
         manager.add(MoscowParis);
         manager.add(MoscowParisSales);
-        manager.add(MoscowParisBusiness);
+        manager.add(MoscowParisPremium);
         manager.add(MoscowPrague);
         manager.add(MoscowPragueSales);
         manager.add(MoscowBeijing);
@@ -50,7 +50,7 @@ class FlightOfferManagerTest {
 
     @Test
     public void shouldFindAllAndSort() {
-        FlightOffer[] expected = new FlightOffer[]{MoscowParisSales, MoscowPrague, MoscowPragueSales, MoscowLondon, MoscowParis, MoscowBeijing, MoscowParisBusiness};
+        FlightOffer[] expected = new FlightOffer[]{MoscowParisSales, MoscowPrague, MoscowPragueSales, MoscowLondon, MoscowParis, MoscowBeijing, MoscowParisPremium};
         FlightOffer[] actual = repository.findAll();
         Arrays.sort(actual);
 
@@ -59,7 +59,7 @@ class FlightOfferManagerTest {
 
     @Test
     public void shouldSearchByIATAAndSort() {
-        FlightOffer[] expected = new FlightOffer[]{MoscowParisSales, MoscowParis, MoscowParisBusiness};
+        FlightOffer[] expected = new FlightOffer[]{MoscowParisSales, MoscowParis, MoscowParisPremium};
         FlightOffer[] actual = manager.findAll("SVO", "PAR");
 
         assertArrayEquals(expected, actual);
